46fd0d1eb5836df1b5122a3b0eac94528e0dbcaa
Jose Oliva Local <jose@joseoliva.com>
2022-10-31 00:29:39 +0100

    Añadidas fuentes y color de texto segun el color de cada tarjeta

--- app/src/main/java/joseoliva/com/postit/adapter/PostitViewHolder.kt
+++ app/src/main/java/joseoliva/com/postit/adapter/PostitViewHolder.kt
@@ -6,13 +6,14 @@ import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import joseoliva.com.postit.R
 import joseoliva.com.postit.bbdd.PostIt
 
 class PostitViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-    var titulo = view.findViewById<TextView>(R.id.TVpostit)
+    val titulo = view.findViewById<TextView>(R.id.TVpostit)
     val fecha = view.findViewById<TextView>(R.id.TVDate)
     val btndelete = view.findViewById<ImageView>(R.id.IVDelete)
     val positfondo = view.findViewById<CardView>(R.id.cardview)
@@ -25,9 +26,16 @@ class PostitViewHolder(view: View): RecyclerView.ViewHolder(view) {
     ){
         titulo.text = positmodel.titulo
         fecha.text = "Actualizado: " + positmodel.fecha
-
+        //cambio la fuente para unos colores u otros y el color del texto
+        if(color.equals("#8BC34A") || color.equals("#00BCD4")){ //para color verde o azul
+            titulo.typeface = ResourcesCompat.getFont(titulo.context,R.font.dancing_normal)
+            titulo.setTextColor(Color.parseColor("#FFFFFFFF"))
+        }else{
+            titulo.typeface = ResourcesCompat.getFont(titulo.context,R.font.hand_normal)
+            titulo.setTextColor(Color.parseColor("#FF000000"))
+        }
         positfondo.setBackgroundColor(Color.parseColor(color))
-        
+
 
         itemView.setOnClickListener {
             onClickListener(positmodel)
